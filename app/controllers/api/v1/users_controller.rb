1a474b005951b9b0a75a22c9e10471e119751250
dharmendra2001 <rathored2001@gmail.com>
2023-03-08 11:45:31 +0530

    association

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::UsersController < ApplicationController
   skip_before_action :verify_authenticity_token
   def index
-    render json:User.all
+    @user = User.all
+    render json: @user
   end
 
   def create
